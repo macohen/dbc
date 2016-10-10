4c2f1c0fa663f3e9a7446d29b7fa93de4090bff2
Mark Cohen <macohen@users.noreply.github.com>
2016-10-10 15:05:25 -0400

    added foreach

--- notebooks/Users/mark.cohen@viacom.com/pipeline-test.scala
+++ notebooks/Users/mark.cohen@viacom.com/pipeline-test.scala
@@ -1,2 +1,3 @@
-// Databricks notebook source exported at Mon, 10 Oct 2016 17:46:12 UTC
+// Databricks notebook source exported at Mon, 10 Oct 2016 19:05:27 UTC
 var testing = "Here's a test for the pipeline"
+testing.foreach(l => println(l))
