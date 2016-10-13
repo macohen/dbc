407a6e46917d1cd2d566f0e9e955618fe4b70ab2
Mark Cohen <macohen@users.noreply.github.com>
2016-10-13 10:35:06 -0400

    Update pipeline-test.scala

--- notebooks/Users/mark.cohen@viacom.com/pipeline-test.scala
+++ notebooks/Users/mark.cohen@viacom.com/pipeline-test.scala
@@ -1,3 +1,3 @@
-// Databricks notebook source exported at Thu, 13 Oct 2016 14:33:50 UTC
+// Databricks notebook source exported at Thu, 13 Oct 2016 14:35:06 UTC
 var testing = "Here's a test for the pipeline"
 testing.foreach(l => println(l))
